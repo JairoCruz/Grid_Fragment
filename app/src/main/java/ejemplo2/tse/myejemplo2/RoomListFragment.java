1b17925714ddc1e6f15bc4ab08751eb4d3f9d935
Jairo Cruz <azaveliz@gmail.com>
2015-05-18 12:18:15 -0600

    modificado RoomGridFragment

--- app/src/main/java/ejemplo2/tse/myejemplo2/RoomListFragment.java
+++ app/src/main/java/ejemplo2/tse/myejemplo2/RoomListFragment.java
@@ -3,7 +3,9 @@ package ejemplo2.tse.myejemplo2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -37,6 +39,10 @@ public class RoomListFragment extends ListFragment {
 
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+       return inflater.inflate(R.layout.fragment_room_list,null);
+    }
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
